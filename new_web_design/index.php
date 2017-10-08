8564ab5ab1d4967c583314f7815870f98dc6dc84
ankm786 <32462580+ankm786@users.noreply.github.com>
2017-10-08 13:51:19 +0530

    Add files via upload

---
+++ new_web_design/index.php
@@ -0,0 +1,235 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+<title>HAPPY MONTESSORI HIGH SCHOOL</title>
+     <meta name="description" content="The mission of the Happy Montessori School is to provide the essentials of quality education for young children:  support, predictability, warmth, and acceptance, in the context of intellectually-stimulating, enriching activities.  We are influenced by the Montessori philosophy, which recommends a structured environment for children to develop to their full potential.">
+  <meta name="author" content="narayan aash">
+      <meta name="viewport" content="width=device-width">
+
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+    
+</head>
+<body>
+    <div class="skul_head">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspHAPPY MONTESSORI HIGH SCHOOL<div class="lower_head">Learn to be happy!</div><div class="logo"><img src="img/logo.png" widh="110px" height="110px"></div><div class="email"><div class="email_logo"><img src="img/email.png" width="20px" height="20px"></div>happyschool1234@gmail.com<br>0361-8759628</div>
+        <div class="phone_logo"><img src="img/phone.png" width="20px" height="20px"></div></div>
+        
+        
+    <div class="hindi_head">हैप्पी मोंटेसरी हाई स्कूल</div> 
+    
+    
+    <h4>BELTOLA CHARIALI GUWAHATI-781029</h4>
+<p class="para1">Welcome to the most prestigious educational institute of beltola</p>
+    <div class="nevigate"><div><a class="nev" href="index.php">Home</a></div><div><a class="nev" href="https://www.facebook.com/Happy-Montessori-English-High-School-1761713414097135/">About</a></div><div><a class="nev" href="dd.html">faculty</a><div><a class="nev"href="dhhd.html">Contact</a></div></div>
+    <div class="date">2017-18</div></div>
+    
+    <div class="make_border">
+    <style>
+
+    #slides {
+      display: none
+    }
+
+    #slides .slidesjs-navigation {
+      margin-top:5px;
+    }
+
+    a.slidesjs-next,
+    a.slidesjs-previous,
+    a.slidesjs-play,
+    a.slidesjs-stop {
+      background-image: url(img/btns-next-prev.png);
+      background-repeat: no-repeat;
+      display:block;
+      width:12px;
+      height:18px;
+      overflow: hidden;
+      text-indent: -9999px;
+      float: left;
+      margin-right:5px;
+    }
+
+    a.slidesjs-next {
+      margin-right:10px;
+      background-position: -12px 0;
+    }
+
+    a:hover.slidesjs-next {
+      background-position: -12px -18px;
+    }
+
+    a.slidesjs-previous {
+      background-position: 0 0;
+    }
+
+    a:hover.slidesjs-previous {
+      background-position: 0 -18px;
+    }
+
+    a.slidesjs-play {
+      width:15px;
+      background-position: -25px 0;
+    }
+
+    a:hover.slidesjs-play {
+      background-position: -25px -18px;
+    }
+
+    a.slidesjs-stop {
+      width:18px;
+      background-position: -41px 0;
+    }
+
+    a:hover.slidesjs-stop {
+      background-position: -41px -18px;
+    }
+
+    .slidesjs-pagination {
+      margin: 7px 0 0;
+      float: right;
+      list-style: none;
+    }
+
+    .slidesjs-pagination li {
+      float: left;
+      margin: 0 1px;
+    }
+
+    .slidesjs-pagination li a {
+      display: block;
+      width: 13px;
+      height: 0;
+      padding-top: 13px;
+      background-image: url(img/pagination.png);
+      background-position: 0 0;
+      float: left;
+      overflow: hidden;
+    }
+
+    .slidesjs-pagination li a.active,
+    .slidesjs-pagination li a:hover.active {
+      background-position: 0 -13px
+    }
+
+    .slidesjs-pagination li a:hover {
+      background-position: 0 -26px
+    }
+
+    #slides a:link,
+    #slides a:visited {
+      color: #333
+    }
+
+    #slides a:hover,
+    #slides a:active {
+      color: #9e2020
+    }
+
+    .navbar {
+      overflow: hidden
+    }
+  </style>
+  <!-- End SlidesJS Optional-->
+
+  <!-- SlidesJS Required: These styles are required if you'd like a responsive slideshow -->
+  <style>
+    #slides {
+      display: none
+    }
+
+    .container {
+      margin: 0 auto
+    }
+
+    /* For tablets & smart phones */
+    @media (max-width: 767px) {
+      body {
+        padding-left: 20px;
+        padding-right: 20px;
+      }
+      .container {
+        width: auto
+      }
+    }
+
+    /* For smartphones */
+    @media (max-width: 480px) {
+      .container {
+        width: auto
+      }
+    }
+
+    /* For smaller displays like laptops */
+    @media (min-width: 768px) and (max-width: 979px) {
+      .container {
+        width: 724px
+      }
+    }
+
+    /* For larger displays */
+    @media (min-width: 1200px) {
+      .container {
+        width: 1170px
+      }
+    }
+  </style>
+  <!-- SlidesJS Required: -->
+</head>
+<body>
+
+  <!-- SlidesJS Required: Start Slides -->
+  <!-- The container is used to define the width of the slideshow -->
+  <div class="container">
+    <div id="slides">
+      <img src="img/example-slide-1.jpg" alt="Photo by: Narayan Aash">
+      <img src="img/example-slide-2.jpg" alt="Photo by: Narayan Aash">
+      <img src="img/example-slide-3.jpg" alt="Photo by: Narayan Aash">
+      <img src="img/example-slide-4.jpg" alt="Photo by: Narayan Aash">
+    </div>
+  </div>
+  <!-- End SlidesJS Required: Start Slides -->
+
+  <!-- SlidesJS Required: Link to jQuery -->
+  <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+  <!-- End SlidesJS Required -->
+
+  <!-- SlidesJS Required: Link to jquery.slides.js -->
+  <script src="js/jquery.slides.min.js"></script>
+  <!-- End SlidesJS Required -->
+
+  <!-- SlidesJS Required: Initialize SlidesJS with a jQuery doc ready -->
+  <script>
+    $(function() {
+      $('#slides').slidesjs({
+        width: 940,
+        height: 528,
+        play: {
+          active: true,
+          auto: true,
+          interval: 4000,
+          swap: true
+        }
+      });
+    });
+  </script>
+    
+    </div>
+    
+    <div class="tarif"><i>"The mission of the Happy Montessori School is to provide the essentials of quality education for young children:  support, predictability, warmth, and acceptance, in the context of intellectually-stimulating, enriching activities.  We are influenced by the Montessori philosophy, which recommends a structured environment for children to develop to their full potential."</i></div>
+    
+    
+    <div class="video"><video controls><source src="video/Happy.mp4"/></video><div class="video_des">Felicitation program to the STAR mark holders of Happy<br> Montessori school by AASU(all assam stutents union)Beltola..Ghy</div></div><br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    
+<div class="bottom">THANK YOU FOR VISITING OUR WEBSITE</div>
+
+</body>
+</html>
